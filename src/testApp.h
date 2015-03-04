e44f0135185170c845714bef5f9218209485997f
Terry Bu <burea1124@gmail.com>
2015-03-04 18:20:29 -0500

    First Commit

---
+++ src/testApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "Clone.h"
+#include "ofxFaceTracker.h"
+#include "ofxFaceTrackerThreaded.h"
+using namespace ofxCv;
+using namespace cv;
+
+#include "ofxFaceTracker.h"
+
+class testApp : public ofBaseApp {
+public:
+    void setup();
+    void update();
+    void draw();
+    void dragEvent(ofDragInfo dragInfo);
+    void loadFace(string face);
+    
+    void keyPressed(int key);
+    
+    ofxFaceTrackerThreaded camTracker;
+    ofVideoGrabber cam;
+    
+    ofxFaceTracker srcTracker;
+    ofImage src;
+    vector<ofVec2f> srcPoints;
+    
+    bool cloneReady;
+    Clone clone;
+    ofFbo srcFbo, maskFbo;
+    
+    ofDirectory faces;
+    int currentFace;
+};
